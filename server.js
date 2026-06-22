34d6e400d0c6920908220d1a2d44aefbef6a12f9
Quincy-Bee <quincybee@icloud.com>
2026-06-22 00:34:17 -0400

    style sheet works

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//static file
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// serve public folder
+app.use(express.static(path.join(__dirname, "public")));
+
 // middleware 
 app.use(express.json());
 
