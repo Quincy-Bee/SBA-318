9f6288834ce79af7b5dc3b43ac3f749d43ab493c
Quincy-Bee <quincybee@icloud.com>
2026-06-21 23:47:08 -0400

    routes created

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+//moddleware 1
+const activityLogger = (req, res, next) => {
+  console.log(`[CRM ACTIVITY] ${req.method} ${req.url}`);
+  next();
+};
+
+//middleware 2
+const leadSourceTagger = (req, res, next) => {
+  req.leadSource = req.query.source || "unknown";
+  next();
+}
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 }); 
